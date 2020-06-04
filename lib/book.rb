f23ec80eeaad3d39c58c2b3a5fcab445dc1b5e0f
Nroulston <nicolas.roulston@gmail.com>
2020-06-04 11:21:30 -0700

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,20 @@
+class Book
+    attr_accessor :author, :page_count, :genre
+   def initialize(title)
+     @title = title
+     @title
+   end
 
+   def title
+       @title
+   end
 
+  
+
+   def turn_page
+    puts "Flipping the page...wow, you read fast!"
+
+   end
+end
+
+# Run only those tests by typing `learn spec/01_book_spec.rb` on your command line.
